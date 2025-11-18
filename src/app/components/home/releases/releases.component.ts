d86a3c13931cf88d4e306a2e21ad3cbfd25ed444
wrincied <wrincied@gmail.com>
2025-11-19 00:50:16 +0100

    app.component update

--- src/app/components/home/releases/releases.component.ts
+++ src/app/components/home/releases/releases.component.ts
@@ -18,25 +18,25 @@ public releaseDB =[
       alt:'Big collection of Business Card Mockups'
   },
   {
-          src:'/assets/img/Knits.jpg',
+          src:'assets/img/Knits.jpg',
       title:'Big collection of paid beautiful and colorful illustrations',
       author:'Krushko Nikita',
       alt:'Big collection of paid beautiful and colorful illustrations'
   },
   {
-          src:'/assets/img/20 funny and carefully-designed scenes in a cartoon style.png',
+          src:'assets/img/20 funny and carefully-designed scenes in a cartoon style.png',
       title:'20 funny and carefully-designed scenes in a cartoon style',
       author:'FelixTrex',
       alt:'Big collection of Business Card Moc20 funny and carefully-designed scenes in a cartoon style'
   },
   {
-          src:'/assets/img/In touch.jpg',
+          src:'assets/img/In touch.jpg',
       title:'Large collection of paid minimalistic illustrations',
       author:'Krushko Nikita',
       alt:'Large collection of paid minimalistic illustrations'
   },
   {
-          src:'/assets/img/UI Illustrations.jpg',
+          src:'assets/img/UI Illustrations.jpg',
       title:'UI Illustrations',
       author:'Author',
       alt:'UI Illustrations'
